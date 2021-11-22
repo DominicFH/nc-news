d16b21c16effcf30a4a82eec6066622f60ae0d63
Dominic <dominicfrancisharris@gmail.com>
2021-11-22 16:57:27 +0000

    Adds basic CSS for Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-        <div className='nav'>
-            <h2>Nav</h2>
-        </div>
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <Link to="/articles" className="nav-button">
+                        Articles
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/users" className="nav-button">
+                        Users
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
