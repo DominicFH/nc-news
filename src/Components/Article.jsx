7750868c777fbe18bf9ce869dea10bef191e9017
Dominic <dominicfrancisharris@gmail.com>
2021-11-24 12:37:05 +0000

    Adds form to add comment

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
-import { getArticle, getArticleComments } from '../Utils/api';
+import { getArticle, getArticleComments, postComment } from '../Utils/api';
 import CommentCard from './CommentCard';
 
 const Article = () => {
     const {article_id} = useParams();
     const [article, setArticle] = useState([]);
     const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState()
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
@@ -14,13 +15,27 @@ const Article = () => {
             setArticle(article[0]);
             setIsLoading(false);
         })
-    }, [article_id])
+    }, [article_id, comments])
 
     useEffect(() => {
         getArticleComments(article_id).then((comments) => {
             setComments(comments)
         })
     }, [article_id])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        postComment(article_id, newComment);
+        setNewComment({})
+    }
+
+    const handleInput = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setNewComment((newCommentObject) => {
+            return { ...newCommentObject, [name]: value };
+        })
+    }
     
     if (isLoading) return <h2>Loading article...</h2>
     return (
@@ -28,6 +43,14 @@ const Article = () => {
             <h2>{article.title}</h2>
             <h3>Votes: {article.votes}</h3>
             <p>{article.body}</p>
+            <h3>Add a comment:</h3>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Enter an existing username: </label>
+                <input type="text" name="username" id="username" onChange={handleInput} required/>
+                <label htmlFor="body">Enter comment: </label>
+                <input type="text" name="body" id="body" onChange={handleInput} required/>
+                <button type="submit">Add Comment</button>
+            </form>
             <h3>Comments:</h3>
             <ul className="all-comments">
                 {comments.map((comment) => {
