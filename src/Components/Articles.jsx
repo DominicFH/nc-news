be8f017bd86d33d445151a8993dca34ba9659bc1
Dominic <dominicfrancisharris@gmail.com>
2021-11-25 17:03:39 +0000

    Begins to implement queries for Articles page

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getArticles } from "../Utils/api.js";
 import ArticleCard from "./ArticleCard"
+import TopicSelector from './TopicSelector.jsx';
 
 const Articles = () => {
     const [articles, setArticles] = useState([]);
+    const [chosenTopic, setChosenTopic] = useState()
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -13,10 +15,15 @@ const Articles = () => {
         })
     }, [])
 
+    if (chosenTopic) {
+        console.log("Truthy!")
+    } else console.log ("Falsy!")
+
     if (isLoading) return <h2>Loading articles...</h2>
     return (
         <div className="content">
             <h2>Articles</h2>
+            <TopicSelector setChosenTopic={setChosenTopic}/>
                 <ul className="all-articles">
                     {articles.map((article) => {
                         return <ArticleCard key={article.article_id} {...article} />
--- src/Components/TopicSelector.jsx
+++ src/Components/TopicSelector.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import { getTopics } from '../Utils/api';
 
-const TopicSelector = () => {
+const TopicSelector = ({setChosenTopic}) => {
+    const [topics, setTopics] = useState([])
+
+    useEffect(() => {
+        getTopics().then((topics) => {
+            setTopics(topics)
+        })
+    }, [])
+
+    const handleClick = (event) => {
+        setChosenTopic(event.target.value)
+    }
+
     return (
         <div>
             <h3>Topic Selector</h3>
+            <ul>
+                {topics.map((topic) => {
+                    return <button onClick={handleClick} value={topic.slug} key={topic.slug}>{topic.slug}</button>
+                })}
+            </ul>
         </div>
     );
 };
