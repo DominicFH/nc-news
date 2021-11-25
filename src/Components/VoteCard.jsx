adc2cf536afab8c0b8f708c90c46b751c741d80c
Dominic <dominicfrancisharris@gmail.com>
2021-11-25 11:49:41 +0000

    Sets up Vote Card base logic

--- src/Components/VoteCard.jsx
+++ src/Components/VoteCard.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const VoteCard = (props) => {
-    console.log(props)
+    let parent
+
+    if (props.article) {
+        parent = props.article;
+    } else parent = props.comment;
+
+
+    const addVote = () => {
+        if (!parent.comment_id) {
+            console.log("Is article")
+        } else console.log("Is not article")
+    }
+
+    const subVote = () => {
+        if (!parent.comment_id) {
+            console.log("Is article")
+        } else console.log ("Not article")
+    }
+
     return (
-        <div>
-            <p>VOTE CARD PLACEHOLDER TEXT</p>
+        <div className="vote-card">
+            <p>VOTE COUNT</p>
+            <button onClick={addVote}>+</button>
+            <button onClick={subVote}>-</button>
         </div>
     );
 };
